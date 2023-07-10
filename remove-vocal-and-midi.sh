6261c5809c0eaa1575343548ccb9d29de800969f
Sangjee Dondrub <sangjeedondrub@live.com>
2023-07-10 20:58:15 +0000

    midi conversion and audio/vocal separation

---
+++ remove-vocal-and-midi.sh
@@ -0,0 +1,11 @@
+for mp3 in data/mp3/*.mp3; do
+    echo "Processing $mp3"
+    # remove vocals
+    spleeter separate -p spleeter:2stems -o data/sep "$mp3"
+    fname="${mp3%.*}"
+    fname=`basename "$fname"`
+    acc="data/sep/$fname/accompaniment.wav"
+    mkdir -p data/midi/$fname
+    basic-pitch data/midi/$fname "$acc"
+    done
+done
